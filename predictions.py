e41acea2a1402ed1c140db3106545761f968f954
Robert Hancock <hancock.robert@gmail.com>
2010-10-21 10:59:28 -0400

    init

---
+++ predictions.py
@@ -0,0 +1,36 @@
+import httplib2
+from urllib import urlencode
+
+CLIENT_LOGIN_URI = "https://www.google.com/accounts/ClientLogin"
+TRAINING_URI = "www.googleapis.com/prediction/v1.1/train/"
+
+h = httplib2.Http(".cache")
+
+#Request auth token
+header = {"Content-Type":"application/x-www-form-urlencoded"}
+data =  {"accountType":"HOSTED_OR_GOOGLE", "Email":"hancock.robert@gmail.com",
+  "Passwd":"w1ls0n1136", "service":"xapi", "source":"account"}
+
+resp, content = h.request(CLIENT_LOGIN_URI, "POST", urlencode(data), headers=header)
+
+# Check code status:200
+if resp["status"] != "200":
+    print ('Response status code {sc}'.format(sc=resp["status"]))
+    return
+
+# ValueError if not found
+try:
+    i = content.rindex('Auth')
+except ValueError:
+    print("Auth not found in content")
+auth = content[i:],strip("\n")
+
+print auth
+
+#Invoke training post
+headers = {"Content-Type":"application/json", "Authorization":"GoogleLogin"}
+data = {
+  -d "{\"data\":{}}"
+  
+  auth=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789...XYZ"
+  https://www.googleapis.com/prediction/v1.1/training?data=mybucket%2F$mydata
