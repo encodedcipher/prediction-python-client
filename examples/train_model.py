484e26918742f3b2652d6a38109e4919a0efaeb4
Robert Hancock <hancock.robert@gmail.com>
2010-10-25 19:31:25 -0400

    Removed fetch_token. Corrected varia ble names from command line. Fixed import statements.

--- examples/train_model.py
+++ examples/train_model.py
@@ -1,11 +1,12 @@
 """ Train a model. """
 import sys
+import os
 import getopt
 from optparse import OptionParser
 
 try:
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-    import prediction
+    from prediction import Prediction, HTTPError
 except ImportError:
     print 'Error importing Prediction library!!'
 
@@ -13,36 +14,23 @@ def main():
     # TODO
     #verify bucket and data - boto?
     # make properties of some elements
-    usage = "%prog email password bucket data"
+    usage = "%prog auth_token bucket data"
     parser = OptionParser(usage)
     parser.add_option("-D", "--debug", dest="debug", action="store_true",
                       help="Write debug to stdout.")
     
     [options, args] = parser.parse_args()
-    if len(args) < 4:
+    if len(args) < 3:
         parser.error('Incorrect number of arguments')
         return -1
     else:
-        email = args[0]
-        password = args[1]
-        bucket = args[2]
-        data = args[3]
+        auth_token = args[0]
+        bucket = args[1]
+        data = args[2]
         
     debug = True if options.debug else False
 
-    p = Prediction(email, password, bucket, data)
-
-    retval = p.fetch_auth_token()
-    if retval["Status"] != "200":
-        if retval.has_key("Error"):
-            error = retval["Error"]
-            #TODO What are the other errors?
-            if Error == "CaptchaRequired":
-                #TODO Deal with captcha
-                #CaptchaToken=DQAAAGgA...dkI1LK9
-                #CaptchaUrl=Captcha?ctoken=HiteT4b0Bk5Xg18_AcVoP6-yFkHPibe7O9EqxeiI7lUSN
-                sys.stderr.write("captcha")
-                return 1
+    p = Prediction(auth_token, bucket, data)
 
     try:
         p.invoke_training()
