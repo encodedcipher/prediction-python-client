c9823c004168b9622a7a3f51d43e60ac4f02e68c
Robert Hancock <hancock.robert@gmail.com>
2010-10-25 11:49:49 -0400

    Removed extraneous code.

--- examples/is_training_complete.py
+++ examples/is_training_complete.py
@@ -17,25 +17,7 @@ def main():
 
     p = Prediction(email, password, bucket, data)
 
-    retval = p.fetch_auth_token()
-    if retval["Status"] != "200":
-        if retval.has_key("Error"):
-            error = retval["Error"]
-            #TODO What are the other errors?
-            if Error == "CaptchaRequired":
-                #TODO Deal with captcha
-                #CaptchaToken=DQAAAGgA...dkI1LK9
-                #CaptchaUrl=Captcha?ctoken=HiteT4b0Bk5Xg18_AcVoP6-yFkHPibe7O9EqxeiI7lUSN
-                sys.stderr.write("captcha")
-                return 1
-
-    try:
-        p.invoke_training()
-    except HTTPError as e:
-        sys.stderr.write("invoke_training returned: {ex}".format(ex=e))
-        return 1
-
-    #TODO Maintain any type of state in a cookie? auth, bucket, data
+     #TODO Maintain any type of state in a cookie? auth, bucket, data
     training_complete = False
     while not training_complete:
         retval =  p.training_complete()
