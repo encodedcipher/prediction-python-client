d17fe61cdfc8535e069d9eed29a42da1a5557774
Robert Hancock <hancock.robert@gmail.com>
2010-10-25 11:51:50 -0400

    Add delete_model.py

---
+++ examples/delete_model.py
@@ -0,0 +1,5 @@
+def delete_model(self):
+    """ Delete a previously trained mode. """
+    #-H "Authorization: GoogleLogin auth=auth-token"
+    #https://www.googleapis.com/prediction/v1.1/training/mybucket%2Fmydata        
+    pass
