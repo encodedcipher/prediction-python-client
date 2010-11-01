a910cc1631cef28a5f8ef03d167d4b85c5400019
Robert Hancock <hancock.robert@gmail.com>
2010-11-01 17:15:26 -0400

    Test through bad prediction.

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -1,16 +1,22 @@
-""" Unit tests for Prediction Python Client """
+""" Unit tests for Prediction Python Client 
+
+Some of these are more functional tests.
+
+"""
 import unittest
 import sys
 import os
 
-
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import prediction as p
+
+#------START--- Change to your valid entries -----------
 good_email = "hancock.robert@gmail.com"
 good_password = "w1ls0n1136"
-boto_config = "../.boto"
+boto_config = "/home/rhancock/.boto"
 good_bucket = "utest"
 good_object = "small"
+#------STOP--- Change to your valid entries -----------
 
 class TestAuth(unittest.TestCase):
     def setUp(self):
@@ -32,7 +38,7 @@ class TestAuth(unittest.TestCase):
         try:
             a = p.Auth(good_email, good_password, botoconfig="badfile")
         except Exception as e:
-            self.assertEqual(type(e), NameError)
+            self.assertEqual(type(e), OSError)
  
 class TestStorage(unittest.TestCase):
     def setUp(self):
@@ -44,14 +50,14 @@ class TestStorage(unittest.TestCase):
                           
     def test_fetch_buckets(self):
         res = self.s.fetch_buckets()
-        self.assertIsInstance(res, list)
+        self.assertEquals(type(res), list)
 
     def test_fetch_buckets_bad(self):
         self.assertRaises(Exception, self.s_bad.fetch_buckets, ())
         
     def test_fetch_objects(self):
         res = self.s.fetch_ojbects(self.buckets[0])
-        self.assertIsInstance(res, list)
+        self.assertEquals(type(res),list)
         
     def test_fetch_objects_bad(self):
         self.assertRaises(Exception, self.s.fetch_ojbects, ('bad_bucket'))
@@ -60,16 +66,57 @@ class TestPrediction(unittest.TestCase):
     def setUp(self):
         global boto_config
         self.auth = p.Auth(good_email, good_password, boto_config)
-        self.auth_bad = 'badauth'
-        self.bucket_bad = "badbucket"
-        self.data_bad = "baddata"
+        self.bad_auth = 'badauth'
+        self.bad_bucket = "badbucket"
+        self.bad_object = "baddata"
         
-    #auth bucket data
+    def test_auth_bad_auth(self):
+        try:
+            pred = p.Prediction(self.bad_auth, good_bucket, good_object)
+        except Exception as e:
+            self.assertEqual(type(e), TypeError)
+    
     def test_auth_bad_bucket(self):
         try:
-            p = p.Prediction(self.auth.token, self.bucket_bad, good_object)
+            pred = p.Prediction(self.auth.token, self.bad_bucket, good_object)
         except Exception as e:
             self.assertEqual(type(e), p.StorageError)
+            
+    def test_auth_bad_object(self):
+        try:
+            pred = p.Prediction(self.auth.token, good_bucket, self.bad_object)
+        except Exception as e:
+            self.assertEqual(type(e), p.StorageError)
+            
+class TestPredict(unittest.TestCase):
+    def setUp(self):
+        self.bad_fmt = "badformat"
+        self.test_data = "test data"
+        self.auth = p.Auth(good_email, good_password)
+        self.pred = p.Prediction(self.auth.token, good_bucket, good_object)
+        
+    def test_predict_bad_format(self):
+        try:
+            self.pred.predict(self.bad_fmt, self.test_data)
+        except Exception as e:
+            self.assertEqual(type(e), ValueError)
+            
+    def test_predict_bad_data(self):
+        try:
+            self.pred.predict("text", 666.0)
+        except Exception as e:
+            self.assertEqual(type(e), ValueError)
+    
+        try:
+            self.pred.predict("numeric", "string")
+        except Exception as e:
+            self.assertEqual(type(e), ValueError)
+            
+    def test_predict_good(self):
+        
+            
+    
+        
                          
 if __name__ == "__main__":
     unittest.main()
