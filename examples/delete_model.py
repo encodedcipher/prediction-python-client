6a29ef216a86e3bc378b8c77308ca4e4aeaea2de
rhancock <rhancock@localhost6.localdomain6>
2010-11-03 20:58:27 -0400

    Trap NotFoundErrort and HTTPError on one line.

--- examples/delete_model.py
+++ examples/delete_model.py
@@ -6,18 +6,13 @@ from optparse import OptionParser
 
 try:
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-    from prediction import Prediction, HTTPError
+    from prediction import Prediction, HTTPError, NotFoundError
 except ImportError:
     print 'Error importing Prediction library!!'
 
 def main():
-    # TODO
-    #verify bucket and data - boto?
-    # make properties of some elements
     usage = "%prog auth_token bucket data"
     parser = OptionParser(usage)
-    parser.add_option("-D", "--debug", dest="debug", action="store_true",
-                      help="Write debug to stdout.")
     
     [options, args] = parser.parse_args()
     if len(args) < 3:
@@ -28,11 +23,15 @@ def main():
         bucket = args[1]
         data = args[2]
         
-    debug = True if options.debug else False
-
     p = Prediction(auth_token, bucket, data)
     
-    p.delete_model
+    try:
+        p.delete_model()
+    except (NotFoundError, HTTPError) as e:
+        print e
+        return 1
+        
+    print("Deleted bucket:{b}, object:{o}".format(b=bucket, o=data))
     
-#An empty response is returned if a model exists and is successfully deleted, 
-#and a 400 error if the model does not exist
+if __name__ == "__main__":
+    sys.exit(main())
