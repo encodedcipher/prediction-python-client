144ddb99214f49cbdf51a5b9de6a271ef3bf4288
Robert Hancock <hancock.robert@gmail.com>
2010-11-07 17:25:19 -0500

    Corrected import statements.

--- examples/allinone.py
+++ examples/allinone.py
@@ -8,21 +8,21 @@ that you specify on the command line with bucket and gsobject.
 
 import sys
 import os
-import getopt
 from optparse import OptionParser
-import traceback
+import time
 
 try:
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-    from prediction import HTTPError, StorageError, TrainingError, NotFoundError
+    import prediction 
 except ImportError:
     print 'Error importing Prediction library!!'
 
     
 def main():
+    default_iterations = 6
+    default_seconds = 30
     usage = "%prog [-Dis] email password bucket gsobject"
     parser = OptionParser(usage)
-    [options, args] = parser.parse_args()
     parser.add_option("-D", "--debug", dest="debug", action="store_true",
                       help="Write debug to stdout.")
     parser.add_option("-i", "--iterations", dest="iterations", action="store",
@@ -31,15 +31,17 @@ def main():
     parser.add_option("-s", "--secs", dest="seconds", action="store",
                       help="Seconds to sleep between iterations.  "
                       +"Default is {s}".format(s=default_seconds))    
-
+    [options, args] = parser.parse_args()
+    
     if len(args) < 4:
         parser.error('Incorrect number of arguments')
     else:
         email = args[0]
         password = args[1]
         bucket = args[2]
-        gsobject = args[4]
+        gsobject = args[3]
     
+    debug = True if options.debug else False
     if options.iterations:
         try:
             iterations = int(options.iterations)
@@ -47,7 +49,7 @@ def main():
             sys.stderr.write("Cannot convert iteration argument {i} to int\n".format(i=options.iterations))
             return 1
     else:
-        iterations = 6
+        iterations = default_iterations
     if debug: print("iterations={i}.".format(i=iterations))
         
     if options.seconds:
@@ -62,11 +64,12 @@ def main():
       
     # Fetch auth token
     try:
-        auth_token = Auth(email, password) 
-        if debug: print('Auth token: {t}'.format(t=token))
-    except HTTPError:
-        if auth.captcha.get('CaptchaToken'):
-            sys.stderr('Deal with captcha!')
+        auth = prediction.Auth(email, password)
+        auth_token = auth.fetch_token()
+        if debug: print('Auth token: {t}'.format(t=auth_token))
+    except prediction.HTTPError:
+        if auth_token.captcha.get('CaptchaToken'):
+            sys.stderr.write('Deal with captcha!')
         else:
             sys.stderr.write("Prediction http status code: {c}".format(c=auth.http_status))
             return 1
@@ -75,12 +78,12 @@ def main():
         return 1
     
     # Get prediction object.
-    p = Prediction(auth_token, bucket, gsobject)
+    p = prediction.Prediction(auth_token, bucket, gsobject)
     
     # Invoke training.
     try:
         p.invoke_training()
-    except HTTPError as e:
+    except prediction.HTTPError as e:
         sys.stderr.write("invoke_training returned: {ex}".format(ex=e))
         return 1
     
@@ -93,7 +96,7 @@ def main():
                                                       x=iterations))        
         try:
             retval = p.training_complete()
-        except HTTPError as e:
+        except prediction.HTTPError as e:
             sys.stderr.write(str(e))
             return 1
         
@@ -106,10 +109,14 @@ def main():
         iteration += 1
     
     if training_complete:
-        if debug: print("Estimated accuracy: {a}".format(a=retval))
+        if debug: print("Training complete: Estimated accuracy: {a}".format(a=retval))
     else:
         if debug: print("Training hasn't completed.  retval={r}".format(r=retval))
         return 1
         
     # Make a prediction.
     resp = p.predict("text", "Is this sentence in english?")
+    print(resp)
+    
+if __name__ == "__main__":
+    sys.exit(main())    
