129d14f7ff42b8fb1316d82992af7d2f5f8bacf5
Robert Hancock <hancock.robert@gmail.com>
2010-11-07 22:10:16 -0500

    Corrected all storage tests.

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -92,7 +92,7 @@ class TestStorage(unittest.TestCase):
                 objs = self.s.fetch_objects(bucket)
                 if objs:
                     for obj in objs:
-                        obj.delete()
+                        self.s.delete_object(bucket, obj)
                 self.s.delete_bucket(bucket)
 
     def test_fetch_buckets(self):
@@ -109,107 +109,117 @@ class TestStorage(unittest.TestCase):
     def test_fetch_objects_bad(self):
         self.assertRaises(Exception, self.s.fetch_objects, ('bad_bucket'))
 
-    #def test_create_bucket(self):
-        #self.s.create_bucket(self.create_bucket_name)
-        #buckets = self.s.fetch_buckets()
-        #self.assertTrue(self.create_bucket_name in buckets)
+    def test_create_bucket(self):
+        self.s.create_bucket(self.create_bucket_name)
+        buckets = self.s.fetch_buckets()
+        self.assertTrue(self.create_bucket_name in buckets)
 
-    #def test_delete_bucket(self):
-        #self.s.delete_bucket(self.delete_me_bucket)
-        #buckets = self.s.fetch_buckets()
-        #self.assertTrue(self.delete_me_bucket not in buckets)
+    def test_delete_bucket(self):
+        self.s.delete_bucket(self.delete_me_bucket)
+        buckets = self.s.fetch_buckets()
+        self.assertTrue(self.delete_me_bucket not in buckets)
 
-    #def test_upload_file(self):
-        #self.s.upload_file(self.test_bucket,self.test_file)
-        #objects = self.s.fetch_objects(self.test_bucket)
-        #self.assertTrue(self.test_file in objects)
+    def test_upload_file(self):
+        self.s.upload_file(self.test_bucket,self.test_file)
+        objects = self.s.fetch_objects(self.test_bucket)
+        self.assertTrue(self.test_file in objects)
 
-#class TestPrediction(unittest.TestCase):
-    #def setUp(self):
-        #global boto_config
-        #self.auth = p.Auth(good_email, good_password, good_boto_config)
-        #self.token = self.auth.fetch_token()
-        #self.bad_auth = 'badauth'
-        #self.bad_bucket = "badbucket"
-        #self.bad_object = "baddata"
+class TestPrediction(unittest.TestCase):
+    def setUp(self):
+        global boto_config
+        self.auth = p.Auth(good_email, good_password, good_boto_config)
+        self.token = self.auth.fetch_token()
+        self.bad_auth = 'badauth'
+        self.bad_bucket = "badbucket"
+        self.bad_object = "baddata"
 
-    #def test_auth_bad_auth(self):
-        #try:
-            #pred = p.Prediction(self.bad_auth, good_bucket, good_object)
-        #except Exception as e:
-            #self.assertEqual(type(e), TypeError)
+    def test_auth_bad_auth(self):
+        try:
+            pred = p.Prediction(self.bad_auth, good_bucket, good_object)
+        except Exception as e:
+            self.assertEqual(type(e), TypeError)
 
-    #def test_auth_bad_bucket(self):
-        #try:
-            #pred = p.Prediction(self.token, self.bad_bucket, good_object)
-        #except Exception as e:
-            #self.assertEqual(type(e), p.StorageError)
+    def test_auth_bad_bucket(self):
+        try:
+            pred = p.Prediction(self.token, self.bad_bucket, good_object)
+        except Exception as e:
+            self.assertEqual(type(e), p.StorageError)
 
-    #def test_auth_bad_object(self):
-        #try:
-            #pred = p.Prediction(self.token, good_bucket, self.bad_object)
-        #except Exception as e:
-            #self.assertEqual(type(e), p.StorageError)
+    def test_auth_bad_object(self):
+        try:
+            pred = p.Prediction(self.token, good_bucket, self.bad_object)
+        except Exception as e:
+            self.assertEqual(type(e), p.StorageError)
 
-#class TestPredict(unittest.TestCase):
-    #def setUp(self):
-        #global p
-        #self.bad_fmt = "badformat"
-        #self.test_data = "test data"
-        #self.auth = p.Auth(good_email, good_password)
-        #self.token = self.auth.fetch_token()
-        #self.pred = p.Prediction(self.token, good_bucket, good_object)
+class TestPredict(unittest.TestCase):
+    def setUp(self):
+        global p
+        self.bad_fmt = "badformat"
+        self.test_data = "test data"
+        self.auth = p.Auth(good_email, good_password)
+        self.token = self.auth.fetch_token()
+        self.pred = p.Prediction(self.token, good_bucket, good_object)
 
-        ## Create bucket and object for delete model test
-        #self.bucketname = "test_predict_dummy_bucket"+str(nonce())
+        # Create bucket and object for delete model test
+        self.bucketname = "test_predict_dummy_bucket"+str(nonce())
         #self.objectname = "test_object_{n}".format(n=self.bucketname)
-        #self.test_file = "test_file_{n}".format(n=nonce())
+        self.test_file = "test_file_{n}".format(n=nonce())
 
-        #with open(self.test_file, 'w') as fh:
-            #fh.write("French, Je suis le roi\n")
-            #fh.write("Italian, Sono il re\n")
-            #fh.write("English, I am the King\n")
+        with open(self.test_file, 'w') as fh:
+            fh.write("French, Je suis le roi\n")
+            fh.write("Italian, Sono il re\n")
+            fh.write("English, I am the King\n")
 
-        #self.a = p.Auth(good_email, good_password).fetch_token()
-        #self.s = p.Storage(self.a)
+        self.auth = p.Auth(good_email, good_password).fetch_token()
+        self.s = p.Storage(self.auth)
 
-        #self.s.create_bucket(self.bucketname)
-        #self.s.upload_file(self.bucketname, self.test_file)
-        #this_p = p.Prediction(self.auth, self.bucketname, self.objectname)
-        #this_p.invoke_training()
-        #while not this_p.training_complete():
-            #time.sleep(2)
+        self.s.create_bucket(self.bucketname)
+        self.s.upload_file(self.bucketname, self.test_file)
+        # TODO Training time is always exceeded.
+        #this_p = p.Prediction(self.auth, self.bucketname, self.test_file)
+        #training_complete = False
+        #max_secs = 60
+        #start_time = time.time()
+        #while not training_complete:
+            #try:
+                #this_p.training_complete()
+            #except p.TrainingError:
+                #if (time.time() - start_time) > 60:
+                    #print("Max training time exceeded.")
+                    #sys.exit()
+                    
+                #time.sleep(2)
 
-    #def tearDown(self):
-        #if os.path.isfile(self.test_file):
-            #os.remove(self.test_file)
+    def tearDown(self):
+        if os.path.isfile(self.test_file):
+            os.remove(self.test_file)
 
-        #buckets = self.s.fetch_buckets()
-        #for bucket in buckets:
-            #if bucket.startswith("test"):
-                #objs = self.s.fetch_objects(bucket)
-                #if objs:
-                    #for obj in objs:
-                        #obj.delete()
-                #self.s.delete_bucket(bucket)
+        buckets = self.s.fetch_buckets()
+        for bucket in buckets:
+            if bucket.startswith("test"):
+                objs = self.s.fetch_objects(bucket)
+                if objs:
+                    for obj in objs:
+                        self.s.delete_object(bucket, obj)
+                self.s.delete_bucket(bucket)
 
 
-    #def test_predict_bad_format(self):
-        #try:
-            #self.pred.predict(self.bad_fmt, self.test_data)
-        #except Exception as e:
-            #self.assertEqual(type(e), ValueError)
+    def test_predict_bad_format(self):
+        try:
+            self.pred.predict(self.bad_fmt, self.test_data)
+        except Exception as e:
+            self.assertEqual(type(e), ValueError)
 
-    #def test_predict_bad_data(self):
-        #try:
-            #self.pred.predict("text", 666.0)
-        #except Exception as e:
-            #self.assertEqual(type(e), ValueError)
+    def test_predict_bad_data(self):
+        try:
+            self.pred.predict("text", 666.0)
+        except Exception as e:
+            self.assertEqual(type(e), ValueError)
 
-        #try:
-            #self.pred.predict("numeric", "string")
-        #except Exception as e:
-            #self.assertEqual(type(e), ValueError)
+        try:
+            self.pred.predict("numeric", "string")
+        except Exception as e:
+            self.assertEqual(type(e), ValueError)
 
     ## To do
     ##def test_predict_good(self):
